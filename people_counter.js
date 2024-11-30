4d0bd5be6cbeac128b35752c991c05fa7f276273
qsadozai <125207185+qsadozai@users.noreply.github.com>
2024-11-30 19:51:38 +0100

    new update version 1.0.1
    added new function called reset !

--- people_counter.js
+++ people_counter.js
@@ -1,7 +1,9 @@
 let eintrittZähler = 0;
 let btn1 = document.getElementById("eintritt_El");
 let btn2 = document.getElementById("zuvor");
+let btn3 = document.getElementById("btn3");
 let hover1 = document.getElementById("btn1");
+let zurücksetzen = 0;
 
 function eingetreten_btn() {
 	eintrittZähler += 1;
@@ -13,4 +15,9 @@ function speichern_btn() {
 	btn2.textContent += aufzählerStrg;
 	aufzählerStrg = 0;
 	eintrittZähler = 0;
+}
+
+function zurücksetzen_btn() {
+	btn1.textContent = zurücksetzen;
+	eintrittZähler = zurücksetzen;
 }
